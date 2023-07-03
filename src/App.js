4e78f3ce38ae2218478d089fb070ec717a9b32ba
Wilfred <wbridges905@gmail.com>
2023-07-03 13:37:11 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Navigate } from "react-router-dom"
+import Root, { ROUTES } from "./components/root/Root";
+import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
+import { ContactsPage } from "./containers/contactsPage/ContactsPage";
+
+function App() {
+  /*
+  Define state variables for 
+  contacts and appointments 
+  */
+  const [contacts, setContacts] = useState([]);
+  const [appointments, setAppointments] = useState([]);
+  
+  /*
+  Implement functions to add data to
+  contacts and appointments
+  */
+
+  //In the callback functions, set the new state with the setter function using the spread operator:
+  //Define a callback function that, given a name, phone number, and email, adds a new contact object with that data to the array of contacts
+  const addContact = (name, phone, email) => {
+    const newContact = {
+      name: name,
+      phone: phone,
+      email: email
+    }
+    setContacts([...contacts, newContact])
+  }
+
+  //Define a callback function that, given a name, contact, date, and time, adds a new appointment object with that data to the array of appointments
+  const addAppointment = (name, contact, date, time) => {
+    const newAppointment = {
+      name: name,
+      contact: contact,
+      date: date,
+      time: time
+    }
+    setAppointments([...appointments, newAppointment])
+  }
+
+  
+
+  
+
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={ <Root/> }>
+      <Route index element={ <Navigate to={ROUTES.CONTACTS} replace/> }/>
+      <Route path={ROUTES.CONTACTS} element={ <ContactsPage contacts={contacts} addContact={addContact}/> }/>
+      <Route path={ROUTES.APPOINTMENTS} element={ <AppointmentsPage appointmets={appointments} contacts={contacts} addAppointment={addAppointment}/>  }/>
+    </Route>
+  ));
+  
+  return (
+    <RouterProvider router={router}/>
+  );
+}
+
+export default App;
---
+++ src/containers/contactsPage/ContactsPage.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+
+import { ContactForm } from "../../components/contactForm/ContactForm";
+import { TileList } from "../../components/tileList/TileList";
+
+export const ContactsPage = () => {
+  /*
+  Define state variables for 
+  contact info and duplicate check
+  */
+  const [contactInfo, setContactInfo] = useState({})
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    /*
+    Add contact info and clear data
+    if the contact name is not a duplicate
+    */
+  };
+
+  /*
+  Using hooks, check for contact name in the 
+  contacts array variable in props
+  */
+
+  return (
+    <div>
+      <section>
+        <h2>Add Contact</h2> 
+      </section>
+      <hr />
+      <section>
+        <h2>C
