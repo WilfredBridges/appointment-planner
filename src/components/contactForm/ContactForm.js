8f72e8832ae50e051146120eca37c39cffe4bbbc
Wilfred <wbridges905@gmail.com>
2023-07-03 14:15:45 +0100

    rendering contact form

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,7 +10,38 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    <></>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Phone:
+        <input
+          type="tel"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" // Example pattern for phone number with hyphens
+        />
+      </label>
+      <br />
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <br />
+      <input type="submit" value="Submit" />
+    </form>
   );
 };
+
 
